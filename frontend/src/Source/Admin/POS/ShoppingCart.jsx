5459c83d89aa4c1b4a784b8eed926a04a3b2f40c
chalankakodikara <ischalankakodikara@gmail.com>
2024-01-10 14:52:45 +0530

    cart modified

--- frontend/src/Source/Admin/POS/ShoppingCart.jsx
+++ frontend/src/Source/Admin/POS/ShoppingCart.jsx
@@ -1,14 +1,43 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 
-export default function ShoppingCart({ cart }) {
+export default function ShoppingCart({ cart, handleOpenModal }) {
   return (
     <div style={{ marginTop: "20px" }}>
-      <Typography variant="h5">Shopping Cartgggg</Typography>
-      {cart.map((item) => (
-        <div key={item.id}>
-          <Typography>{item.name}</Typography>
-          <Typography>Price: {item.price}</Typography>
+      {cart.map((product) => (
+        <div key={product.id}>
+          <Card
+            sx={{
+              display: "flex",
+              maxWidth: 500,
+              margin: 5,
+            }}
+            onClick={() => handleOpenModal(product)}
+          >
+            <CardMedia
+              component="img"
+              alt={product.name}
+              height="100"
+              src={`data:image/jpeg;base64,${product.image}`}
+              sx={{
+                width: 100,
+                flexShrink: 0,
+              }}
+            />
+            <CardContent sx={{
+              flex: 1,
+            }}>
+              <Typography gutterBottom variant="h8" component="div">
+                {product.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Price: {product.price}
+              </Typography>
+            </CardContent>
+          </Card>
           <hr />
         </div>
       ))}
